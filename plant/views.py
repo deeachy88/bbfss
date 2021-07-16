797908342bb5e114563197d2564cf133f609a0df
san-gurung <gurungsan@gmail.com>
2021-07-16 11:22:03 +0600

    git commit -m "update"

--- plant/views.py
+++ plant/views.py
@@ -7,7 +7,6 @@ from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template.loader import render_to_string
 from django.utils import timezone
-import requests
 
 from administrator.models import t_dzongkhag_master, t_gewog_master, t_village_master, t_location_field_office_mapping, \
     t_user_master, t_field_office_master, t_plant_crop_master, t_plant_pesticide_master, t_plant_crop_variety_master, \
